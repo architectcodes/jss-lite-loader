653379f937588ed0a6392a94faf93caa1f4eafb8
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2016-03-16 21:22:02 +0100

    Test logic

--- test.js
+++ test.js
@@ -1,13 +1,37 @@
 const test = require('ava');
+const requireFromString = require('require-from-string');
+const cssInJs = require('css-in-js');
 
 const cssInJsLoader = require('.');
 
 const mockLoaderApi = (mocks) => (
   Object.assign({
-    exec: () => ({}),
+    exec: source => requireFromString(source),
     cacheable: () => {},
     resourcePath: '/any/file.js',
   }, mocks)
+);
+
+test(
+  'Returns a module exporting a string of CSS',
+  (assert) => {
+    const styleObject = { '.a': { margin: '0' } };
+    const result = requireFromString(
+      cssInJsLoader.apply(mockLoaderApi(), [
+        `module.exports = ${JSON.stringify(styleObject)};`,
+      ])
+    );
+
+    assert.is(
+      typeof result,
+      'string'
+    );
+
+    assert.is(
+      result,
+      cssInJs(styleObject)
+    );
+  }
 );
 
 test(
